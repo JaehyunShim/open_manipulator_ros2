bb0a16a99a58a73bd2feb9aa290d4adf8cacc143
Ryan Shim <jhshim@robotis.com>
2019-08-14 18:06:54 +0900

    shell script file added

---
+++ install_ros_dashing.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2017, ROBOTIS CO., LTD.
+
+echo ""
+echo "[Note] Target OS version  >>> Ubuntu 18.04 (Bionic Beaver) or Linux Mint 19.x"
+echo "[Note] Target ROS version >>> ROS Dashing Diademata"
+echo "[Note] Robotis workspace   >>> $HOME/robotis_ws"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Set the target OS, ROS version and name of robotis workspace]"
+name_ros_version=${name_ros_version:="dashing"}
+name_robotis_workspace=${name_robotis_workspace:="robotis_ws"}
+
+echo "[Install ROS2]"
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo apt install ros-dashing-desktop
+sudo apt install python3-argcomplete python3-colcon-common-extensions python3-vcstool
+source /opt/ros/$name_ros_version/setup.sh
+
+echo "[Make the robotis workspace and test colcon build]"
+mkdir -p $HOME/$name_robotis_workspace/src
+cd $HOME/$name_robotis_workspace/src
+git clone https://github.com/ROBOTIS-Platform/ros2_examples.git
+cd $HOME/$name_robotis_workspace
+colcon build --symlink-install
+
+echo "[Set the ROS evironment]"
+sh -c "echo \"alias gb='gedit ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+sh -c "echo \"alias cw='cd ~/$name_robotis_workspace'\" >> ~/.bashrc"
+sh -c "echo \"alias cs='cd ~/$name_robotis_workspace/src'\" >> ~/.bashrc"
+sh -c "echo \"alias cb='cd ~/$name_robotis_workspace && colcon build --symlink-install'\" >> ~/.bashrc"
+sh -c "echo \"alias cr='cd ~/$name_robotis_workspace && rm -R build install log'\" >> ~/.bashrc"
+sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_robotis_workspace/install/local_setup.bash\" >> ~/.bashrc"
+
+source $HOME/.bashrc
+
+echo "[Complete!!!]"
+exit 0
